c69db00e41e7f06d9767edbcc65bae590e085ccd
Subhash Umarane <Subhash Umarane@Subhash>
2025-02-27 19:45:01 +0530

    Second commit

--- FrameWork01/src/main/java/object/repository/PageObjectModel.java
+++ FrameWork01/src/main/java/object/repository/PageObjectModel.java
@@ -4,6 +4,7 @@ public class PageObjectModel {
 
 	public static void main(String[] args) {
 		System.out.println("Page object Model");
+		System.out.println("Fresh Token added on 27/02/2025");
 
 	}
 
---
+++ FrameWork01/src/main/java/object/repository/PageObjectModelPracticeForTcs.java
@@ -0,0 +1,10 @@
+package object.repository;
+
+public class PageObjectModelPracticeForTcs {
+
+	public static void main(String[] args) {
+		System.out.println("Fresh Token Added on 27-02-2025");
+
+	}
+
+}
